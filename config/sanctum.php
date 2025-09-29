b1bc6cca2bcfb6606e8a99bc74270cdb3ac6f6c5
bovimacoco <www.clintonboniface@gmail.com>
2025-09-29 14:47:56 +0100

    cores settings

--- config/sanctum.php
+++ config/sanctum.php
@@ -12,7 +12,15 @@ return [
     | and production domains which access your API via a frontend SPA.
     |
     */
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,exeat.vercel.app,attendance.veritas.edu.ng')),
+    'stateful' => [
+        'localhost',
+        'localhost:3000', 
+        '127.0.0.1',
+        '127.0.0.1:8000',
+        '::1',
+        'exeat.vercel.app',
+        'attendance.veritas.edu.ng'
+    ],
 
     /*
     |--------------------------------------------------------------------------
